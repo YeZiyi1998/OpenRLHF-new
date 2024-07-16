3bd988fed9e3b1e2fc7235abe0ecd4e76fcb2ec7
Xiaonan Nie <xiaonan.nie@pku.edu.cn>
2024-07-16 19:46:41 +0800

    master

--- run_scripts/64/train_2_rm_s2_c.sh
+++ run_scripts/64/train_2_rm_s2_c.sh
@@ -13,14 +13,16 @@ data_path="/data2/rlhf/yzy/data/rm/pairwise_critic_train_guide_qwen_300k"
 
 # model_path="/data2/rlhf/lixs/open_models/DeepSeek-V2-Lite-Chat"
 model_path="/data2/rlhf/lixs/open_models/Qwen2-7B-Instruct/"
-BATCH_SIZE_PER_GPU=2
+BATCH_SIZE_PER_GPU=1
 LR=9e-6
 
 #/eb_data
 save_path="/data2/rlhf/yzy/OpenRLHF-new/outputs/reward_models/$EXP_NAME"
 ckpt_path="/data2/rlhf/yzy/OpenRLHF-new/outputs/reward_models/$EXP_NAME/checkpoints"
-hostfile="/data2/rlhf/yzy/OpenRLHF-new/run_scripts/0625/hostfile_96"
-WORLD_SIZE=8
+hostfile="./hostfile_64"
+MACHINE_SIZE=$(wc -l < $hostfile)
+WORLD_SIZE=$[MACHINE_SIZE * 8]
+# WORLD_SIZE=8
 
 #TODO-be-check: bs large, adamw
 GLOBAL_BATCH_SIZE=$(( ${BATCH_SIZE_PER_GPU} * ${WORLD_SIZE} ))
@@ -28,6 +30,7 @@ GLOBAL_BATCH_SIZE=$(( ${BATCH_SIZE_PER_GPU} * ${WORLD_SIZE} ))
 mkdir -p $save_path
 mkdir -p $ckpt_path
 
+# train critic
 read -r -d '' training_commands <<EOF
 ../../examples/train_2rm.py \
      --save_path $save_path \
@@ -51,13 +54,58 @@ read -r -d '' training_commands <<EOF
      --use_wandb $WANDB_TOKENS \
      --wandb_run_name $EXP_NAME \
      --wandb_project rl_critic_test \
-     --mode s2_c
+     --mode c
 EOF
-     # --wandb [WANDB_TOKENS] or True (use wandb login command)
-     # --train_batch_size 128 \
-     #--flash_attn \
-        #--compute_fp32_loss \
-        #     --adam_offload \
+deepspeed --hostfile $hostfile $training_commands 2>&1 | tee $save_path/train_qwen.log
 
-deepspeed $training_commands 2>&1 | tee $save_path/train_qwen.log
-# python /data2/rlhf/yzy/write_email.py --info "$EXP_NAME done"
+# MODEL=""
+# # inference critic
+# read -r -d '' training_commands <<EOF
+# ../../examples/batch_inference.py \
+#     --eval_task generate \
+#     --pretrain $MODEL \
+#     --bf16 \
+#     --max_len 6400 \
+#     --dataset $DATA \
+#     --dataset2 $DATA2 \
+#     --dataset_probs 1.0 \
+#     --zero_stage 0 \
+#     --tp_size 4 \
+#     --greedy_sampling \
+#     --micro_batch_size $BATCH_SIZE_PER_GPU \
+#     --flash_attn \
+#     --max_new_tokens 128 \
+#     --input_template Human \
+#     --output_path $save_path/$File_NAME
+# EOF
+
+# deepspeed $training_commands 2>&1 | tee $save_path/train_qwen_inference.log
+
+
+# # train reward model
+# read -r -d '' training_commands <<EOF
+# ../../examples/train_2rm.py \
+#      --save_path $save_path \
+#      --logging_steps 10 \
+#      --micro_train_batch_size $BATCH_SIZE_PER_GPU \
+#      --train_batch_size $GLOBAL_BATCH_SIZE \
+#      --pretrain $model_path \
+#      --quantized_type bf16 \
+#      --max_epochs 1 \
+#      --max_len 2048 \
+#      --zero_stage 3 \
+#      --l2 0.0001 \
+#      --eval_steps 250 \
+#      --save_steps 500 \
+#      --learning_rate $LR\
+#      --dataset $data_path \
+#      --dataset_probs 1 \
+#      --ckpt_path $ckpt_path \
+#      --flash_attn \
+#      --gradient_checkpointing \
+#      --use_wandb $WANDB_TOKENS \
+#      --wandb_run_name $EXP_NAME \
+#      --wandb_project rl_critic_test \
+#      --mode s2
+# EOF
+# deepspeed $training_commands 2>&1 | tee $save_path/train_qwen.log
